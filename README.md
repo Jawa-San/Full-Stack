f1bed973bf939e6c3a89880340a5bac7a99da0f6
Jawa <joshuasingh076@gmail.com>
2023-02-03 17:09:29 -0500

    HUGE CHANGES;

---
+++ README.md
@@ -0,0 +1,4 @@
+# Full-Stack
+This fullstack project is a contact manager.
+When a user first accesses the website, they can either login or sign up (register). Once they are logged in with a valid account, they can perform standard CRUD operations on their list
+of contacts. Users will also be able to search their contacts.
---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- HTML Codes by Quackit.com -->
+<title>
+LeBorn Jahmes</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {background-color:#ffffff;background-image:url(https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png);background-repeat:no-repeat;background-position:center center;background-attachment:fixed;}
+h1{font-family:Arial, sans-serif;color:#850035;background-color:#ffffff;}
+p {font-family:Georgia, serif;font-size:14px;font-style:normal;font-weight:normal;color:#000000;background-color:#ffffff;}
+</style>
+</head>
+<body>
+<h1>SUCCESS</h1>
+<p>I wanted to just have anything here at all lol! Yo mama fat af lmfao</p>
+<p class="link"><a href="logout.php">Click to LogOut</a></p>
+</body>
+</html>
